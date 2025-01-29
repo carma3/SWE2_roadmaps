e31b921654a1b69e2776e50853d22a866de5d36c
Gmodery <moderyg1@montclair.edu>
2025-01-29 15:45:11 -0500

    db start

--- roadmap-app/roadmap_app/roadmaps/models.py
+++ roadmap-app/roadmap_app/roadmaps/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Roadmap(models.Model):
+    roadmap_id = models.AutoField(primary_key=True)
+    roadmap_title = models.CharField(max_length=25)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
