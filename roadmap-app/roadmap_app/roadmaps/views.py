8b8a1b2e375e5d15bc2e0eec3bc52bb10572e8dd
Gmodery <moderyg1@montclair.edu>
2025-01-30 22:12:31 -0500

    start views template

--- roadmap-app/roadmap_app/roadmaps/views.py
+++ roadmap-app/roadmap_app/roadmaps/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Roadmap, Student
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Home Page!")
+
+def roadmap_list(request):
+    roadmaps = Roadmap.objects.all() # Gets all roadmaps from the db
+    return render(request, 'roadmap_list.html', {"roadmaps" : roadmaps})
+    # render syntax: 
+    # render(HTTP request object, 
+    # html file in templates directory, 
+    # dictionary of data to pass to the template ({variable_name : roadmaps list}) )
 
